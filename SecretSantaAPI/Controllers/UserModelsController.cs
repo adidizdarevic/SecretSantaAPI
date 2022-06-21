0e8fb1093871f3dc515f7368eeac5dbdc6fecc72
Adi <adizdarevi1@etf.unsa.ba>
2022-06-21 20:17:55 +0200

    -API routes -Models -Database -Route for pairing users

--- SecretSantaAPI/Controllers/UserModelsController.cs
+++ SecretSantaAPI/Controllers/UserModelsController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecretSantaAPI.Data;
+using SecretSantaAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SecretSantaAPI.Controllers
 {
-    [EnableCors("CorsPolicy")]
     [Route("api/[controller]")]
     [ApiController]
     public class UserModelsController : ControllerBase
     {
-        //private readonly DataContext _context;
+        private readonly DataContext _context;
+        public UserModelsController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> Get()
+        {
+            return Ok(await _context.UserModels.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<UserModel>>> AddUser(UserModel user)
+        {
+            _context.UserModels.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.UserModels.ToListAsync());
+        }
     }
 }
